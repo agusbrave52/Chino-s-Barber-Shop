153dd78768fabcf6136da008b43fab6b50175a15
agusbrave52 <urubravo52@gmail.com>
2023-08-17 13:58:07 -0300

    cambios

--- php/enviar.php
+++ php/enviar.php
@@ -1,11 +1,11 @@
 <?php 
-	$nombre = $_POST['bb-name'];
-    $phone = $_POST['bb-phone'];
-    $time = $_POST['bb-time'];
-    $tipo = $_POST['bb-branch'];
-	$date = $_POST['bb-date'];
+	$nombre = $_POST['nombre'];
+    $phone = $_POST['telefono'];
+    $time = $_POST['hora'];
+    $tipo = $_POST['corte'];
+	$date = $_POST['fecha'];
 	$asunto = 'Reserva lugar';
-	$mensaje = "Nombre: ".$nombre."<br> Telefono: $phone<br> Hora: $time <br> Fecha: $date <br> Tipo: $tipo <br> Mensaje:".$_POST['bb-message'];
+	$mensaje = "Nombre: " + $nombre + "<br> Telefono: " + $phone + " <br> Hora: " + $time + " <br> Fecha: " + $date + " <br> Tipo: " + $tipo + " <br> Mensaje:" + $_POST['mensaje'];
 
 
 	if(mail('urubravo52@gmail.com', $asunto, $mensaje)){
